da16739c69b7dfad77850332bbf58f626d63c51d
TimoFidel <61385072+TimoFidel@users.noreply.github.com>
2020-10-17 08:41:13 +0530

    created

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from flask import Flask, request, url_for,render_template
+from flask_pymongo import PyMongo,MongoClient
+
+app=Flask(__name__)
+client=MongoClient("mongodb://localhost:27017/")
+db=client["register"]
+users=db["users"]
+
+
+@app.route('/',methods=['GET','POST'])
+def search():
+    return " hello"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
